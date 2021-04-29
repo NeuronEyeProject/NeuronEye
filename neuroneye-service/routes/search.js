e83353af7fdbed18795f73de16cc6c8d2775556a
ookamicodes <ruben.iannelli@wehntal.schule>
2021-04-29 22:32:25 +0200

    Database Search v1

--- neuroneye-service/routes/search.js
+++ neuroneye-service/routes/search.js
@@ -2,14 +2,14 @@ const URLList = require("../models/urlSchema");
 const request = require('request');
 const chalk = require('chalk');
 const url = require('url');
+const fetch = require("node-fetch");
 
 var express = require('express');
 var router = express.Router();
 
-let foundshort = false;
+let foundshort = false
 let shortprov = ["bit.ly", "goo.gl", "tinyurl.com", "ow.ly", "is.gd", "buff.ly", "bit.do", "auf.ly", "mcaf.ee", "su.pr", "bl.ink", "tiny.one", "moourl.comm"];
 
-//  if (message.content.toLowerCase().includes(ignoreWord[i].toLowerCase())) foundInTextWord = true;
 
 ////////////////////////////
 //                        //
@@ -18,27 +18,32 @@ let shortprov = ["bit.ly", "goo.gl", "tinyurl.com", "ow.ly", "is.gd", "buff.ly",
 ////////////////////////////
 
 /* <-- URL by domain --> */
-router.get('/scan', (req, res, next) => {
+router.get('/scan', async (req, res, next) => {
 
   const reqUrl = req.query.url
 
-if (shortprov.includes(reqUrl)) {
-   // req to expander
-   // then
-   // get data req
-   // else then
-}
+  async function databaseSearch () {
 
+    for (var i in shortprov){
+      if (reqUrl.toLowerCase().includes(shortprov[i].toLowerCase())) foundshort = true;
+  }
+  if(foundshort = true){ 
+    const api_url = `http://localhost:3000/api/v1/url/expander?url=${reqUrl}`
+    const fetch_res = await fetch(api_url)
+    const json_res = await fetch_res.json()
 
-  let urlObject = reqUrl.parse(address, true);
+    let endsDomain = json_res.domain /// end data
+    let endURL = json_res.url /// end data
+   
+  }
 
-  const domainObject = urlObject.hostname
+  const domainObject = endsDomain
 
-    URLList.find({ domain: `${domainObject}`})
+  URLList.find({ domain: `${domainObject}`})
       .exec()
-      .then(ifFoundID => {
-        if (ifFoundID) {
-          res.status(200).json(ifFoundID, { found: true } );
+      .then(ifFound => {
+        if (ifFound) {
+          res.status(200).json(ifFound, { found: true } );
         } else {
             res.status(404).json({ message: "No Link found from the provided Domain", found: false });
         }
@@ -46,6 +51,13 @@ if (shortprov.includes(reqUrl)) {
       .catch(err => {
         res.status(500).json({ error: err });
   });
+  }
+
+  databaseSearch()
+
+
+
+
 });
 
 
