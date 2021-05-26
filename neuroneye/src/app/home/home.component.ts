bd70378d445b17f585d985ba46587ba010bd9c51
ookamicodes <ruben.iannelli@wehntal.schule>
2021-05-26 17:29:11 +0200

    contact del

--- neuroneye/src/app/home/home.component.ts
+++ neuroneye/src/app/home/home.component.ts
@@ -11,16 +11,14 @@ export class HomeComponent {
   /** Based on the screen size, switch from standard to one column per row */
 
   contentA = "NeuronEye is a project started in spring 2021. NeuronEye is a Web Application where you can check if the domain respectively the website is contained of cyberthreats. We are trying to make a new OSINT tool for security researches. We have a database that will updated with time. The web application also offers a user friendly interface for desktop user. The Webapp is in early access and will not be the greates from the design. If you want to contribute to the designs, feel free to do it." // ATheProject
-  contentB = "Soon....." // ATheDev
-  contentC = "Soon....." // Source
+
 
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
           { content: this.contentA ,title: 'About This Project', cols: 2, rows: 1 },
-          { content: this.contentB ,title: 'About The Developer', cols: 2, rows: 1 },
-          { content: this.contentC ,title: 'Source Collection', cols: 2, rows: 1 },
+
 
  
         ];
@@ -28,9 +26,7 @@ export class HomeComponent {
 
       return [
         { content: this.contentA ,title: 'About This Project', cols: 2, rows: 1 },
-        { content: this.contentB ,title: 'About The Developer', cols: 2, rows: 1 },
-        { content: this.contentC ,title: 'Source Collection', cols: 2, rows: 1 },
-
+    
       ];
     })
   );
